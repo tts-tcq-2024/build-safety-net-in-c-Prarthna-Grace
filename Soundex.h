cd2dfd5267403547530c9c094388880a8282fb82
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-09-05 16:01:43 +0530

    Update Soundex.h
    updated the conditions

--- Soundex.h
+++ Soundex.h
@@ -1,40 +1,53 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
 
-#include "Soundex.h"
 #include <ctype.h>
 #include <string.h>
 
 char getSoundexCode(char c) {
     c = toupper(c);
     switch (c) {
-        case 'W': case 'H': case 'Y': return '7';
+        case 'H': case 'W': return '7';  // Ignored characters, but impact separation logic
         case 'B': case 'F': case 'P': case 'V': return '1';
         case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
         case 'D': case 'T': return '3';
         case 'L': return '4';
         case 'M': case 'N': return '5';
         case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U
+        default: return '0';  // For vowels (A, E, I, O, U)
     }
 }
 
 void generateSoundex(const char *name, char *soundex) {
-    soundex[0] = toupper(name[0]);
+    soundex[0] = toupper(name[0]);  // Keep the first letter unchanged
     int sIndex = 1;
+    int len = strlen(name);
 
-    for (int i = 1; i < strlen(name) && sIndex < 4; i++) {
-        char code = getSoundexCode(name[i]);
-        if (code != '7' && code != soundex[sIndex - 1]) {
-            soundex[sIndex++] = code;
+    char prevCode = getSoundexCode(name[0]);  // Track previous code
+    for (int i = 1; i < len && sIndex < 4; i++) {
+        char currentChar = name[i];
+        char currentCode = getSoundexCode(currentChar);
+
+        if (currentCode == '0') {  // Skip vowels, but continue to next
+            prevCode = '0';  // Reset prevCode since vowels separate similar codes
+            continue;
+        }
+
+        if (currentCode == '7') {  // If H or W, check the next character but don’t add
+            continue;
+        }
+
+        if (currentCode != prevCode) {  // If the current code differs from the previous, add it
+            soundex[sIndex++] = currentCode;
+            prevCode = currentCode;
         }
     }
 
     while (sIndex < 4) {
-        soundex[sIndex++] = '0';
+        soundex[sIndex++] = '0';  // Pad with zeros if needed
     }
 
-    soundex[4] = '\0';
+    soundex[4] = '\0';  // Null-terminate the string
 }
 
 #endif // SOUNDEX_H
