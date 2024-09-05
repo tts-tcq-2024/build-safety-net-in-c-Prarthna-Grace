af11a04a2c99005f26364c214d5f44b412f65be7
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-09-05 15:47:13 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -8,14 +8,14 @@
 char getSoundexCode(char c) {
     c = toupper(c);
     switch (c) {
-        case 'W': case 'H': return '7';
+        case 'W': case 'H': case 'Y': return '7';
         case 'B': case 'F': case 'P': case 'V': return '1';
         case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
         case 'D': case 'T': return '3';
         case 'L': return '4';
         case 'M': case 'N': return '5';
         case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, Y
+        default: return '0'; // For A, E, I, O, U
     }
 }
 
