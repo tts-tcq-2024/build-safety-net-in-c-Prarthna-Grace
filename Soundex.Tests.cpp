6f078ce88711eebd3a314026d18f3f250c097a3b
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-09-10 12:42:21 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -6,6 +6,15 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
   char soundex[5];
   generateSoundex("Robert", soundex);
   ASSERT_STREQ(soundex,"R163");
+  
+  generateSoundex("Euler", soundex);
+  ASSERT_STREQ(soundex,"E460");
+
+  generateSoundex("A", soundex);
+  ASSERT_STREQ(soundex,"A000");
+
+  generateSoundex(" ", soundex);
+  ASSERT_STREQ(soundex,"0000");
  
  // ASSERT_STREQ("Robert", "R163");
 
