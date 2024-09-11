9b64327e493c5d91d0447ff2f1fc66dd0ba8f30d
Prarthna-Grace <prarthnagrace31@gmail.com>
2024-09-11 18:11:26 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,25 +1,30 @@
 #include <gtest/gtest.h>
-#include "Soundex.h"
+#include <cstring>
+#include "Soundex.h"  // Include your Soundex header file
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
-  char soundex[5];
-  generateSoundex("Robert", soundex);
-  ASSERT_STREQ(soundex,"R163");
-  generateSoundex("ac", soundex);
-  ASSERT_STREQ(soundex,"A200");
-  generateSoundex("ad", soundex);
-  ASSERT_STREQ(soundex,"A300");
-  generateSoundex("af", soundex);
-  ASSERT_STREQ(soundex,"A100");
- 
- // ASSERT_STREQ("Robert", "R163");
+class SoundexTest : public ::testing::TestWithParam<std::tuple<std::string, std::string>> {
+};
 
-}
-TEST(SoudexTestSuite, FirstCharacterStays) {
-  char soundex[5];
-  
-  generateSoundex("aeiouy", soundex);
-  ASSERT_STREQ(soundex,"A000");
+TEST_P(SoundexTest, CorrectSoundexEncoding) {
+    auto [input, expected] = GetParam();
+    char result[MAX_CODE_LENGTH + 1];
+    generateSoundex(input.c_str(), result);
+    EXPECT_EQ(expected, std::string(result));
 }
 
+INSTANTIATE_TEST_SUITE_P(
+    SoundexTests,
+    SoundexTest,
+    ::testing::Values(
+        std::make_tuple("A", "A000"),         // test soundex to pad with zeros for single character
+        std::make_tuple("Rohit", "R300"),     // test soundex to replace consonants with digit code  
+        std::make_tuple("A1CDFe", "A231"),    // test soundex to check Non alphabetic character 
+        std::make_tuple("B[", "B000"),        // test soundex to handle special character 
+        std::make_tuple("1234", "000"),       // test soundex to handle Numerics 
+        std::make_tuple("@@[shr12n", "S650"), // test soundex to find first valid alphabetic character
+        std::make_tuple("12shr3n", "S650"),   // test soundex to handle Numerics 
+        std::make_tuple("Abfp", "A100"),      // test soundex to replace adjacent same digit code only once
+        std::make_tuple("BShZ", "B200"),      // test soundex to replace characters with the same number separated by "h", "w" or "y" are coded as a single number
+        std::make_tuple("", "")               //test soundex for empty
+    )
+);
